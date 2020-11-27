6843c146f4896d015c9e18b8de043d6f0cba1633
Martin Sone <kwangmart12@gmail.com>
2020-11-27 18:29:12 +0100

    build-lightoutgame

---
+++ precache-manifest.de89eaca131551d58097258fa9b68500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae1e278ad39ffc150a82",
+    "url": "/static/css/main.03940cef.chunk.css"
+  },
+  {
+    "revision": "ae1e278ad39ffc150a82",
+    "url": "/static/js/main.ae1e278a.chunk.js"
+  },
+  {
+    "revision": "a6f3a2216de1c9165847",
+    "url": "/static/js/1.a6f3a221.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e08a3b402e83b70a5eb4712f3e3c6e22",
+    "url": "/index.html"
+  }
+];
